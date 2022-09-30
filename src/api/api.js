41fcf24d051893f09abd8cf43b0e49508a22829e
Rodrigo Dias <rodrigovicente.dev@gmail.com>
2022-09-30 17:34:22 -0300

    deploy

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const apiURls = {
   development: "http://localhost:4000",
-  production: "LINK DO SERVIDOR DEPLOYADO VAI AQUI!!!!!!",
+  production: "https://receitalab.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURls[process.env.NODE_ENV] });
